996b6e7f9d9a99df6c93be560e756073ce73829a
maanadoona <maanadoona@gmail.com>
2019-05-14 01:21:34 +0900

    modify parameter

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -14,9 +14,12 @@ class Controller(object):
         # TODO: Implement
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
 
-        kp = 0.3
-        ki = 0.1
-        kd = 0.
+        //kp = 0.3
+        kp = 0.8
+        //ki = 0.1
+        ki = 0.005
+        //kd = 0.
+        kd = 0.3
         mn = 0.
         mx = 0.2
         self.throttle_controller = PID(kp, ki, kd, mn, mx)
@@ -29,7 +32,7 @@ class Controller(object):
         self.fuel_capacity = fuel_capacity
         self.brake_deadband = brake_deadband
         self.decel_limit = decel_limit
-        self.accel_limit = decel_limit
+        self.accel_limit = accel_limit
         self.wheel_radius = wheel_radius
 
         self.last_time = rospy.get_time()
