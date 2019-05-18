6c3af4f2bab15197976fb0df91d8eca85822bc2e
maanadoona <maanadoona@gmail.com>
2019-05-18 17:34:01 +0900

    pid value

--- ros/src/twist_controller/pid.py
+++ ros/src/twist_controller/pid.py
@@ -9,37 +9,20 @@ class PID(object):
         self.kp = kp
         self.ki = ki
         self.kd = kd
+        self.min = mn
+        self.max = mx
 
-        self.diff = 0.0
-        self.int_cte = 0.0
-        self.prev_cte = 0.0
-        self.steer = 0.0
+        self.int_val = self.last_error = 0.
 
     def reset(self):
-        self.int_cte = 0.0
-        self.prev_cte = 0.0
+        self.int_val = 0.0
 
     def step(self, error, sample_time):
-        self.diff = self.error - self.prev_cte
-        self.int_cte += error
 
-        self.steer = -self.kp*error - self.kd*self.diff - self.ki*self.int_cte
-
-        self.prev_cte = error
-
-        if self.steer > 1:
-            self.steer = 1
-
-        if self.steer < -1:
-            self.steer = -1
-
-        return self.steer
-        '''
         integral = self.int_val + error * sample_time;
-        derivative = (error - self.last_error) / sample_time if sample_time > 0  else 0;
+        derivative = (error - self.last_error) / sample_time;
 
-        y = self.kp * error + self.ki * self.int_val + self.kd * derivative;
-        val = max(self.min, min(y, self.max))
+        val = self.kp * error + self.ki * integral + self.kd * derivative;
 
         if val > self.max:
             val = self.max
@@ -50,4 +33,3 @@ class PID(object):
         self.last_error = error
 
         return val
-        '''
