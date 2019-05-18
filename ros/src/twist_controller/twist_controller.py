016300930637034c317e4b603d6a90fccde4b525
maanadoona <maanadoona@gmail.com>
2019-05-18 17:07:51 +0900

    lowpass - steering

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -52,6 +52,7 @@ class Controller(object):
         self.last_time = current_time
 
         throttle = self.throttle_controller.step(vel_error, sample_time)
+        throttle = self.vel_lpf.filt(throttle)
         brake = 0
 
         if linear_vel == 0. and current_vel < 0.1:
